bef4009c4ad1e942697f1b30a35fdba9f80f1f0d
mgJin <jmg940701@gmail.com>
2023-06-17 19:52:59 +0900

    StudyAboutTest

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,224 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from . import models
+from users.models import User
 
-# Create your tests here.
+
+class TestAmenities(APITestCase):
+    NAME = "am test"
+    DESC = "am des"
+    URL = "/api/v1/rooms/amenities/"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_all_amenities(self):
+        response = self.client.get(self.URL)
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "not 200",
+        )
+
+        self.assertIsInstance(
+            data,
+            list,
+            "not list",
+        )
+        self.assertEqual(len(data), 1)
+        self.assertEqual(
+            data[0]["name"],
+            self.NAME,
+        )
+        self.assertEqual(
+            data[0]["description"],
+            self.DESC,
+        )
+
+    def test_create_amenity(self):
+        new_am_name = "new am"
+        new_des_name = "new desc"
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "name": new_am_name,
+                "description": new_des_name,
+            },
+        )
+
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "not 200",
+        )
+
+        self.assertEqual(
+            data["name"],
+            new_am_name,
+            "not eq name",
+        )
+
+        self.assertEqual(
+            data["description"],
+            new_des_name,
+            "not eq des",
+        )
+
+        response = self.client.post(self.URL)
+        data = response.json()
+        self.assertEqual(
+            response.status_code,
+            400,
+            "not 400",
+        )
+
+        self.assertIn(
+            "name",
+            data,
+            "not in",
+        )
+
+
+class TestAmenity(APITestCase):
+    NAME = "test am"
+    DESC = "test DESC"
+    URL = "/api/v1/rooms/amenities/1"
+
+    def setUp(self):
+        models.Amenity.objects.create(name=self.NAME, description=self.DESC)
+
+    def test_amenity_not_found(self):
+        response = self.client.get("/api/v1/rooms/amenities/2")
+
+        self.assertEqual(
+            response.status_code,
+            404,
+            "not 404",
+        )
+
+    def test_get_amenity(self):
+        response = self.client.get(self.URL)
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "not 200",
+        )
+
+        data = response.json()
+
+        self.assertEqual(
+            data["name"],
+            self.NAME,
+            "not eq name",
+        )
+
+        self.assertEqual(
+            data["description"],
+            self.DESC,
+            "not eq desc",
+        )
+
+    def test_put_amenity(self):
+        put_name = "gimo"
+        put_desc = "pretty"
+        failure_name = "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"
+        # 정상적
+        response = self.client.put(
+            self.URL,
+            data={
+                "name": put_name,
+                "description": put_desc,
+            },
+        )
+
+        self.assertEqual(response.status_code, 200, "not 200")
+        data = response.json()
+        self.assertEqual(
+            data["name"],
+            put_name,
+            "not eq name",
+        )
+        self.assertEqual(
+            data["description"],
+            put_desc,
+            "not eq desc",
+        )
+        # 빈 것 보냈을 때
+        rseponse = self.client.put(self.URL)
+        data_1 = rseponse.json()
+        self.assertIn(
+            "name",
+            data,
+            "not in",
+        )
+        # 글자수 제한 넘길 때
+        response = self.client.put(
+            self.URL,
+            data={
+                "name": failure_name,
+                "description": put_desc,
+            },
+        )
+
+        self.assertEqual(response.status_code, 400, "not 200 in fail")
+
+        # 일부만 보냈을 때
+        response = self.client.put(
+            self.URL,
+            data={
+                "description": put_desc,
+            },
+        )
+
+        data = response.json()
+        self.assertEqual(response.status_code, 200, "not 200")
+        self.assertEqual(
+            data["description"],
+            put_desc,
+            "not eq desc",
+        )
+
+    def test_delete_amentiey(self):
+        response = self.client.delete(self.URL)
+
+        self.assertEqual(
+            response.status_code,
+            204,
+            "not 204",
+        )
+
+
+class TestRooms(APITestCase):
+    URL = "/api/v1/rooms/"
+
+    def setUp(self):
+        user = User.objects.create(
+            username="test",
+        )
+
+        user.set_password("1234")
+        user.save()
+
+        self.user = user
+
+    def test_create_room(self):
+        response = self.client.post(self.URL)
+
+        self.assertEqual(response.status_code, 403, "not 403")
+
+        self.client.force_login(
+            self.user,
+        )
+        response = self.client.post(self.URL)
+
+        data = response.json()
+
+        print(data)
--- rooms/views.py
+++ rooms/views.py
@@ -2,9 +2,14 @@ from django.db import transaction
 from django.utils import timezone
 from django.conf import settings
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError, PermissionDenied
+from rest_framework.exceptions import (
+    NotFound,
+    NotAuthenticated,
+    ParseError,
+    PermissionDenied,
+)
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Amenity, Room
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
@@ -12,8 +17,7 @@ from categories.models import Category
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer,CreateRoomBookingSerializer
-
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 
 class Amenities(APIView):
@@ -28,7 +32,7 @@ class Amenities(APIView):
             amenity = serializer.save()
             return Response(AmenitySerializer(amenity).data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
 class AmenityDetail(APIView):
@@ -56,7 +60,7 @@ class AmenityDetail(APIView):
                 AmenitySerializer(updated_amenity).data,
             )
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         amenity = self.get_object(pk)
@@ -65,25 +69,22 @@ class AmenityDetail(APIView):
 
 
 class Rooms(APIView):
-
     permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         all_rooms = Room.objects.all()
         serializer = RoomListSerializer(
-            all_rooms,
-            many=True,
-            context={"request":request}
+            all_rooms, many=True, context={"request": request}
         )
 
         return Response(serializer.data)
 
     def post(self, request):
-        
         serializer = RoomDetailSerializer(
             data=request.data,
         )
         if serializer.is_valid():
-            #request에서 category의 id를 받음
+            # request에서 category의 id를 받음
             category_pk = request.data.get("category")
             if not category_pk:
                 raise ParseError("Category is required")
@@ -91,7 +92,7 @@ class Rooms(APIView):
                 category = Category.objects.get(
                     pk=category_pk,
                 )
-                #카테고리의 kind가 room이 아니라면 에러
+                # 카테고리의 kind가 room이 아니라면 에러
                 if category.kind == Category.CategoryKindChoices.EXPERIENCES:
                     raise ParseError("Category kind should be rooms")
             except Category.DoesNotExist:
@@ -102,9 +103,9 @@ class Rooms(APIView):
                         owner=request.user,
                         category=category,
                     )
-                    #save한 후에 받음으로써 방을 만들 떄 꼭 필요하지는 않게 만듦
-                    amenities= request.data.get("amenities")
-            
+                    # save한 후에 받음으로써 방을 만들 떄 꼭 필요하지는 않게 만듦
+                    amenities = request.data.get("amenities")
+
                     for amenity_pk in amenities:
                         amenity = Amenity.objects.get(pk=amenity_pk)
                         room.amenities.add(amenity)
@@ -113,11 +114,10 @@ class Rooms(APIView):
             except Exception:
                 raise ParseError("Amennnniitttyy")
         else:
-            return Response(serializer.errors)
-        
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
 
 class RoomDetail(APIView):
-    
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_object(self, pk):
@@ -130,213 +130,185 @@ class RoomDetail(APIView):
         room = self.get_object(pk)
         serializer = RoomDetailSerializer(
             room,
-            context={"request":request},
-            )
+            context={"request": request},
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
         room = self.get_object(pk)
-       
+
         if request.user != room.owner:
             raise PermissionDenied
-        
+
         serializer = RoomDetailSerializer(
-            room, 
-            data = request.data,
+            room,
+            data=request.data,
             partial=True,
-            )
+        )
         if serializer.is_valid():
-            
             if request.data.get("category"):
                 try:
                     category_pk = request.data.get("category")
-                    if Category.objects.get(pk = category_pk):
-                        category = Category.objects.get(pk = category_pk)
+                    if Category.objects.get(pk=category_pk):
+                        category = Category.objects.get(pk=category_pk)
                         if category.kind != Category.CategoryKindChoices.ROOMS:
                             raise ParseError("Room category kind must be rooms")
 
-                        room=serializer.save(category = category)
+                        room = serializer.save(category=category)
                 except Category.DoesNotExist:
                     raise NotFound
             else:
                 room = serializer.save()
-            
+
             if request.data.get("amenities"):
                 amenities_pk_list = request.data.get("amenities")
                 room.amenities.clear()
                 try:
                     for amenity_pk in amenities_pk_list:
-                        amenity = Amenity.objects.get(pk = amenity_pk)
+                        amenity = Amenity.objects.get(pk=amenity_pk)
                         room.amenities.add(amenity)
-                    
+
                 except Amenity.DoesNotExist:
                     pass
-            return Response(
-                        RoomDetailSerializer(room).data
-            )
+            return Response(RoomDetailSerializer(room).data)
         else:
-            return Response(
-                serializer.errors
-            )
-   
-    #delete를 할때는 유저가 로그인 됐는지, 유저가 방주인이랑 같은지 확인해야함
-    def delete(self,request,pk):
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    # delete를 할때는 유저가 로그인 됐는지, 유저가 방주인이랑 같은지 확인해야함
+    def delete(self, request, pk):
         room = self.get_object(pk)
-       
+
         if room.owner != request.user:
             raise PermissionDenied
 
         room.delete()
-        return Response(
-            status= HTTP_204_NO_CONTENT
-        )
+        return Response(status=HTTP_204_NO_CONTENT)
 
 
 class RoomReviews(APIView):
-
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_object(self,pk):
+    def get_object(self, pk):
         try:
             return Room.objects.get(pk=pk)
         except Room.DoesNotExist:
             raise NotFound
 
-    def get(self,request,pk):
+    def get(self, request, pk):
         try:
-            page = request.query_params.get("page",1)
-            page= int(page)
-        except ValueError: #page가 숫자로 바꿀 수 없다면 error
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:  # page가 숫자로 바꿀 수 없다면 error
             page = 1
         page_size = settings.PAGE_SIZE
-        start = (page-1) * page_size
+        start = (page - 1) * page_size
         end = start + page_size
         room = self.get_object(pk)
         serializer = ReviewSerializer(
             room.reviews.all()[start:end],
             many=True,
         )
-        return Response(
-            serializer.data
-        )
-    
-    def post(self,request,pk):
-        serializer = ReviewSerializer(data = request.data)
+        return Response(serializer.data)
+
+    def post(self, request, pk):
+        serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
-            #직접입력하지 않고 자동으로 필요한 항목들이 들어가게
+            # 직접입력하지 않고 자동으로 필요한 항목들이 들어가게
             review = serializer.save(
-                user= request.user,
-                room = self.get_object(pk),
+                user=request.user,
+                room=self.get_object(pk),
             )
             serializer = ReviewSerializer(review)
-            return Response(
-                serializer.data
-            )
+            return Response(serializer.data)
 
 
 class RoomAmenities(APIView):
-
-    def get_object(self,pk):
+    def get_object(self, pk):
         try:
             room = Room.objects.get(pk=pk)
             return room
         except Room.DoesNotExist:
             raise NotFound
-    
-    def get(self,request,pk):
+
+    def get(self, request, pk):
         try:
-            page=  request.query_params.get("page")
+            page = request.query_params.get("page")
             page = int(page)
-        except (ValueError,TypeError):
+        except (ValueError, TypeError):
             page = 1
-      
+
         page_size = 5
-        start = (page-1)*page_size
-        end = start+page_size
+        start = (page - 1) * page_size
+        end = start + page_size
         room = self.get_object(pk)
         amenities = room.amenities.all()[start:end]
         serializer = AmenitySerializer(
             amenities,
             many=True,
         )
-        return Response(
-            serializer.data
-        )
+        return Response(serializer.data)
 
 
 class RoomPhotos(APIView):
-
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_object(self,pk):
+    def get_object(self, pk):
         try:
             return Room.objects.get(pk=pk)
         except Room.DoesNotExist:
             raise NotFound
 
-    def post(self,request,pk):
+    def post(self, request, pk):
         room = self.get_object(pk)
-       
+
         if request.user != room.owner:
             raise PermissionDenied
-        serializer = PhotoSerializer(data = request.data)
+        serializer = PhotoSerializer(data=request.data)
         if serializer.is_valid():
-            photo = serializer.save(
-                room = room
-            )
-            serializer= PhotoSerializer(photo)
-            return Response(
-                serializer.data
-            )
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.data)
         else:
-            return Response(
-                serializer.errors
-            )
-        
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
 
 class RoomBookings(APIView):
-
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get_object(self,pk):
+    def get_object(self, pk):
         try:
             return Room.objects.get(pk=pk)
         except:
             raise NotFound
 
-    def get(self,request,pk):
+    def get(self, request, pk):
         room = self.get_object(pk)
         now = timezone.localtime(timezone.now()).date()
 
         bookings = Booking.objects.filter(
-            room = room,
-            kind = Booking.BookingKindChoices.Room,
-            check_in__gt = now
+            room=room, kind=Booking.BookingKindChoices.Room, check_in__gt=now
         )
-        #만약 bookings 에 유저가 실존하는 room만을 보내줄 것이라고 생각한다면
-        #bookings = Booking.objects.filter(room__pk = pk)만 해줘도 된다
-        #다른건 get_object나 room을 따로 찾아주지 않고 방법이 있다는 뜻
-        serializer = PublicBookingSerializer(bookings,many =True,)
-        return Response(
-            serializer.data
-        )        
+        # 만약 bookings 에 유저가 실존하는 room만을 보내줄 것이라고 생각한다면
+        # bookings = Booking.objects.filter(room__pk = pk)만 해줘도 된다
+        # 다른건 get_object나 room을 따로 찾아주지 않고 방법이 있다는 뜻
+        serializer = PublicBookingSerializer(
+            bookings,
+            many=True,
+        )
+        return Response(serializer.data)
 
-    def post(self,request,pk):
+    def post(self, request, pk):
         room = self.get_object(pk)
-        serializer = CreateRoomBookingSerializer(data = request.data)
+        serializer = CreateRoomBookingSerializer(data=request.data)
         if serializer.is_valid():
-            #createserializer에는 checkin과 checkout, guests 뿐이라 추가를 해줘야한다
+            # createserializer에는 checkin과 checkout, guests 뿐이라 추가를 해줘야한다
             booking = serializer.save(
-                room = room,
-                user= request.user,
-                kind = Booking.BookingKindChoices.Room,
+                room=room,
+                user=request.user,
+                kind=Booking.BookingKindChoices.Room,
             )
             serializer = PublicBookingSerializer(booking)
-            return Response(
-                serializer.data
-            )
-            
+            return Response(serializer.data)
+
         else:
-            return Response(
-                serializer.errors
-            )
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
